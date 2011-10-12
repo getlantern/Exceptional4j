19989a2e1a009a7bb8163935bf97a51051196b83
Adam Fisk <a@littleshoot.org>
2011-10-11 23:40:37 -0700

    Added logging event to callback

--- src/main/java/org/bns/getexceptional4j/GetExceptionalAppenderCallback.java
+++ src/main/java/org/bns/getexceptional4j/GetExceptionalAppenderCallback.java
@@ -1,5 +1,6 @@
 package org.bns.getexceptional4j;
 
+import org.apache.log4j.spi.LoggingEvent;
 import org.json.simple.JSONObject;
 
 /**
@@ -12,8 +13,10 @@ public interface GetExceptionalAppenderCallback {
      * data or edit existing data prior to the exception being reported.
      * 
      * @param json The data for submission.
+     * @param le The data about the logging event, allowing you to not submit
+     * the log if desired.
      * @return <code>true</code> if the bug should be submitted, otherwise
      * <code>false</code>.
      */
-    boolean addData(JSONObject json);
+    boolean addData(JSONObject json, LoggingEvent le);
 }
